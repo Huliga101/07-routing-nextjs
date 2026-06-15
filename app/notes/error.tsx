f79d6d9f20d5bf851f155494c7556659c32add3d
Daniil_Renhevych19 <renden20012@gmail.com>
2026-06-15 10:25:17 +0200

    Fix modal preview and remove obsolete files

--- app/notes/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-type NotesErrorProps = {
-  error: Error;
-};
-
-export default function NotesError({ error }: NotesErrorProps) {
-  return <p>Could not fetch the list of notes. {error.message}</p>;
-}
